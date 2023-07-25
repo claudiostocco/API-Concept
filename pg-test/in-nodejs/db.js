0102d04368abe28cd05d0b77800bb93df6b551e6
claudiostocco <claudiostocco@gmail.com>
2023-07-25 07:55:53 -0300

    Testing PG access in JS.

--- pg-test/in-nodejs/db.js
+++ pg-test/in-nodejs/db.js
@@ -3,28 +3,69 @@ async function connect() {
         return global.connection.connect();
  
     const { Pool } = require('pg');
+    console.log("Criou pool de conexões no PostgreSQL!");
     const pool = new Pool({
         connectionString: process.env.CONNECTION_STRING
     });
- 
-    //apenas testando a conexão
-    const client = await pool.connect();
-    console.log("Criou pool de conexões no PostgreSQL!");
- 
-    const res = await client.query('SELECT NOW()');
-    console.log(res.rows[0]);
-    client.release();
  
     //guardando para usar sempre o mesmo
     global.connection = pool;
     return pool.connect();
 }
 
-async function selectCadCli() {
+async function disconnect() {
     const client = await connect();
-    const res = await client.query('SELECT * FROM CLIENTE');
-    console.log(res.rows[0]);
     client.release();
 }
 
-module.exports = { connect, selectCadCli };
+async function selectNow() {
+    const client = await connect();
+    //apenas testando a conexão   
+    const res = await client.query('SELECT NOW()');
+    client.release();
+    return res.rows[0];
+}
+
+async function selectCadCli(sql) {
+    const client = await connect();
+    const res = await client.query(sql);
+    client.release();
+    return res.rows;
+}
+
+async function insert(table, entitie){
+    // const client = await connect();
+    // ${entitie.keys()}
+
+    // let fields = '';
+    // let values = '';
+    // for (key in entitie) {
+    //     const value = typeof(entitie[key]) == 'number' ? entitie[key] : `"${entitie[key]}"`;
+    //     if (fields) {
+    //         fields = `${fields}, ${key}`;
+    //         values = `${values}, ${value}`;
+    //     } else {
+    //         fields = key;
+    //         values = value;
+    //     }
+    // }
+    // console.log(fields);
+    // console.log(values);
+    // const sql = `INSERT INTO ${table} (${fields}) VALUES (${values})`;
+
+    const fields = [];
+    const values = [];
+
+    for (key in entitie) {
+        values.push(entitie[key]);
+        fields.push(key);
+    }
+    const params = values.map<String>((v,i) => `$${i}`).toString();
+    const sql = `INSERT INTO ${table} (${fields.toString()}) VALUES (${params})`;
+    console.log(sql);
+    // const sql = 'INSERT INTO clientes(nome,idade,uf) VALUES ($1,$2,$3);';
+    // const values = [customer.nome, customer.idade, customer.uf];
+    // return await client.query(sql, values);
+}
+
+module.exports = { selectNow, selectCadCli, insert };
--- pg-test/in-nodejs/index.js
+++ pg-test/in-nodejs/index.js
@@ -3,6 +3,24 @@ require("dotenv").config();
 const db = require("./db");
 
 // db.connect().then((client) => client.release());
+async function start() {
+    now = await db.selectNow();
+    console.log('Agora:', now, '\n');
+    // console.log('');
 
-db.selectCadCli();
+    res = await db.selectCadCli('SELECT * FROM CLIENTE');
+    console.log('Tabela CADCLI:', res);
 
+}
+
+db.insert('CADCLI', {
+        id: '30341446874',
+        nome: 'CLAUDIO MARCIO STOCCO',
+        endereco: 'CAIXA POSTAL',
+        numero: 72,
+        cep: '18769899',
+        idmunicipio: 35,
+        nascimento: "1982-04-23T03:00:00.000Z"
+    }
+)
+// start();
